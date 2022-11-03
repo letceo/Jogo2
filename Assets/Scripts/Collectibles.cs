40a44479a0ba0fa2563237fbe04f72d5e821a989
Letícia Oliveira <95142341+letceo@users.noreply.github.com>
2022-11-03 13:27:22 +0000

    Collectibles are destroyed hehe

---
+++ Assets/Scripts/Collectibles.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    public GameInfo gameInfo;
+    private bool isColliding=false;
+
+//se estiver perto do collectible e interagir faz coisa
+    void Update()
+    {
+        if (isColliding && Input.GetMouseButtonUp(0)) 
+        {
+            if(gameObject.name == "fish")
+            {
+                gameInfo.caughtFish = true;
+                Debug.Log(gameInfo.caughtFish);
+                Destroy(gameObject);
+            }
+
+            if(gameObject.name == "book2")
+            {
+                gameInfo.getBook = true;
+                Debug.Log(gameInfo.getBook);
+                Destroy(gameObject);
+            }
+
+            if(gameObject.name == "football")
+            {
+                gameInfo.fetchBall = true;
+                Debug.Log(gameInfo.fetchBall);
+                Destroy(gameObject);
+            }
+        
+            
+        }
+    }
+
+//chegou perto do collectible ou saiu de perto do collectible?
+    private void OnTriggerEnter(Collider other)
+    {
+        if  (other.tag=="Player")
+        {
+            isColliding=true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if  (other.tag=="Player")
+        {
+            isColliding=false;
+        }
+    }
+}
+
+
+
+//adicionar variavel publica de gameinfo 
+//alterar destroy para o if(gameobject.name = "fish") gameinfo.caughtFish = true; e faço isto para os diferentes objetos no game info. Criar outro script com outro nome(NPC) onde faço a cena para dar a quest e gameinfo.caughtFish que é a mesma do outro, verifica se apanhoue tem o contador 
+//game info - wallet= wallet+1
---
+++ Assets/Scripts/GameInfo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New Game Info")]
+public class GameInfo : ScriptableObject
+
+
+
+{
+    public bool caughtFish = false;
+    public bool getBook = false;
+    public bool fetchBall = false;
+}
